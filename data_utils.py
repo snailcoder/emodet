45db08bbc2910e5208f5354b8d75edcbfd30435a
Yan <yanwong@126.com>
2020-12-16 10:13:41 +0800

    build w2v

--- data_utils.py
+++ data_utils.py
@@ -3,14 +3,83 @@
 # File              : data_utils.py
 # Author            : Yan <yanwong@126.com>
 # Date              : 03.12.2020
-# Last Modified Date: 15.12.2020
+# Last Modified Date: 16.12.2020
 # Last Modified By  : Yan <yanwong@126.com>
 
 import logging
 import glob
 import re
+import collections
 
+import numpy as np
 import tensorflow as tf
+
+def load_vocab(vocab_file):
+  """ Load vocab as an ordered dict.
+
+  Args:
+    vocab_file: The vocab file in which each line is a single word.
+
+  Returns:
+    An ordered dict of which key is the word and value is id.
+
+  """
+  vocab = collections.OrderedDict()
+
+  with tf.io.gfile.GFile(vocab_file, 'r') as f:
+    wid = 0
+    for line in f:
+      w = line.strip()
+      vocab[w] = wid
+      wid += 1
+    logging.info('# Total words in vocab: %d', wid)
+  return vocab
+
+def build_w2v(fname, vocab):
+  """ Build embedding lookup table for words in vocab.
+
+  Args:
+    fname: The pre-trained word vector file, e.g. GloVe.
+    vocab: A dict of words appearing in training corpus.
+
+  Returns:
+    A dict of pre-trained word vectors for words in vocab.
+  """
+  w2v = {}
+  with tf.io.gfile.GFile(fname, 'r') as f:
+    for line in f:
+      fields = line.split()
+      w, v = fields[0], fields[1:]
+      if w in vocab:
+        w2v[w] = np.array(v).astype(np.float)
+
+  logging.info('%d words in vocab are assigned pretrained embeddings', len(w2v))
+
+  return w2v
+
+def build_vocab_embeddings(w2v, vocab, d_word):
+  """Assign word embedding for each word in vocab. For the word that's
+     in vocab but there's no corresponding pre-trained embedding, generate a
+     embedding randomly for it.
+
+  Args:
+    w2v: A dict contains pre-trained word embedding. Each word in this
+      dict is also in the vocab.
+    vocab: An ordered dict of which key is the word and value is id.
+    d_word: The dimension of word embeddings.
+
+  Returns:
+    A word embedding list contains all words in vocab.
+  """
+  embeddings = []
+  for word in vocab:
+    emb = w2v.get(word, None)
+    if emb is None:
+      logging.warning('Word not in vocab: %s', word)
+      emb = np.random.uniform(-0.25, 0.25, d_word)
+    embeddings.append(emb)
+
+  return np.array(embeddings)
 
 def clean_str(string):
   """
