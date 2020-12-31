c4fad9a114bafd3663632831759cb4eab7e6ed53
Yan <yanwong@126.com>
2020-12-31 22:17:45 +0800

    implement cnn model

--- emotion_model.py
+++ emotion_model.py
@@ -3,7 +3,7 @@
 # File              : emotion_model.py
 # Author            : Yan <yanwong@126.com>
 # Date              : 03.12.2020
-# Last Modified Date: 30.12.2020
+# Last Modified Date: 31.12.2020
 # Last Modified By  : Yan <yanwong@126.com>
 
 import tensorflow as tf
@@ -33,13 +33,57 @@ class ContextFreeModel(tf.keras.Model):
 
     batch_size, dial_len, sent_len = x.shape
 
-    x = tf.reshape(x, [-1, sent_len])  # (batch_size, dial_len * sent_len)
+    x = tf.reshape(x, [-1, sent_len])  # (batch_size * dial_len, sent_len)
     x = self.utterance_encoder(x, training, mask)  # (batch_size * dial_len, d_sent)
     x = tf.reshape(x, [batch_size, dial_len, -1])  # (batch_size, dial_len, d_sent)
     
     logits = self.dense(x)
 
     return logits
+
+class CnnModel(tf.keras.Model):
+  def __init__(self, vocab_size, filters, kernel_sizes,
+               d_word, n_classes, rate=0.1, embedding=None):
+    super(CnnModel, self).__init__()
+
+    if embedding is not None:
+      self.embedding = tf.keras.layers.Embedding(
+          vocab_size,
+          d_word,
+          embeddings_initializer=tf.keras.initializers.Constant(embedding))
+    else:
+      self.embedding = tf.keras.layers.Embedding(vocab_size, d_word)
+
+    self.convs = [tf.keras.layers.Conv1D(filters, h, activation='relu')
+                  for i, h in enumerate(kernel_sizes)]
+
+    self.dense = tf.keras.layers.Dense(
+        n_classes,
+        activation='relu',
+        kernel_initializer=tf.keras.initializers.he_uniform())
+    self.dropout = tf.keras.layers.Dropout(rate)
+
+  def call(self, x, training, mask=None):
+    # x.shape == (batch_size, dial_len, sent_len)
+    # mask.shape == (batch_szie, dial_len, sent_len)
+
+    batch_size, dial_len, sent_len = x.shape
+
+    x = tf.reshape(x, [-1, sent_len])  # (batch_size * dial_len, sent_len)
+
+    x = self.embedding(x)
+    # each has shape of (batch_size * dial_len, seq_len - kernel_size + 1, filters)
+    conved = [conv(x) for conv in self.convs]
+    # each has shape of (batch_size * dial_len, 1, filters)
+    pooled = [tf.nn.max_pool1d(c, c.shape[1], c.shape[1], 'VALID') for c in conved]
+    # each has shape of (batch_size * dial_len, filters)
+    pooled = [tf.squeeze(p) for p in pooled]
+    pooled = tf.concat(pooled, axis=1)  # (batch_size * dial_len, len(pooled) * filters)
+    
+    # logits.shape == (batch_size * dial_len, n_classes)
+    logits = self.dense(self.dropout(pooled, training=training))
+
+    return tf.reshape(logits, [-1, dial_len, n_classes])
 
 class BiLstmModel(tf.keras.Model):
   def __init__(self, utter_encoder, units, rate, n_classes):
